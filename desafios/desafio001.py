5e7e9bcf88d2de26271ed6e914167dfe7741f179
rpinetti <robertopinetti@gmail.com>
2024-03-09 01:27:20 -0300

    Desafios da Aula 4

---
+++ desafios/desafio001.py
@@ -0,0 +1,4 @@
+print("========== DESAFIO 001 ==========")
+nome = input("Qual é o seu nome? ")
+print("Olá" + nome + "! prazer em te conhecer")
+print("=================================")
---
+++ desafios/desafio002.py
@@ -0,0 +1,7 @@
+print("========== DESAFIO 002 ==========")
+dia = int(input("Dia = "))
+mes = input("Mês = ")
+ano = int(input("Ano = "))
+
+print("Eu nasci no dia", dia, "de", mes, "de", ano, ". Correto?")
+print("=================================")
